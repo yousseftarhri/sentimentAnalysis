7a7eb988601f17fdd0181e274446289110849d28
yousseftarhri <yousseftarhri@gmail.com>
2024-10-04 14:35:32 +0100

    data scrapped

--- scrap_data.py
+++ scrap_data.py
@@ -9,16 +9,19 @@ from selenium.webdriver.firefox.service import Service
 import requests
 
 Options = Options()
+Options.add_argument('--headless=new')
 
 service = Service("/snap/bin/firefox.geckodriver")
 driver = webdriver.Firefox(service=service, options=Options)
 #
 # Initialize CSV file and write headers (if needed)
+
+
 with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(["ID", "review_comment","review_rating"])
 
-    link_products = "https://www.amazon.com/s?k=sustainability+products&crid=1DOPVMDCV2ZGV&sprefix=sustainability%2Caps%2C256&ref=nb_sb_ss_ts-doa-p_1_14"
+    link_products = "https://www.amazon.com/s?k=sustainability+products&i=hpc&rh=n%3A3760901%2Cp_72%3A1248903011&s=relevancerank&dc&page=3&crid=36Y5KUOCJB4E2&qid=1727982665&rnid=1248901011&sprefix=%2Caps%2C257&ref=sr_pg_3"
     driver.get(link_products)
     response = requests.get(link_products)
     time.sleep(3)
@@ -28,7 +31,6 @@ with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
     id_review = 0
 
     for i in racine:
-        id_review = id_review + 1
 
         div = i.find("div", class_="a-section a-spacing-base")
         link_product = div.find('a', href=True)['href']
@@ -42,74 +44,13 @@ with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
         time.sleep(3)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        link_review = soup.find("div", {"id": "reviews-medley-footer"}).find('a', href=True)['href']
-
-        link_review = f"https://www.amazon.com{link_review}"
-
-        driver.get(link_review)
-        print(f"link review is {link_review}")
-
-        time.sleep(3)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-        racine_ = soup.findAll("div", class_="a-section review aok-relative")
+        racine_ = soup.find("div", class_="a-section review-views celwidget")
         for a in racine_:
-            review_comment = a.find("span", class_="a-size-base review-text review-text-content").text
-            review_rating = a.find("div", class_="a-row").find("span", class_="a-icon-alt").text
+            id_review = id_review + 1
+
+            review_comment = a.find("div",class_="a-expander-content reviewText review-text-content a-expander-partial-collapse-content").text
+            review_rating = a.find("span", class_="a-icon-alt").text
 
             writer.writerow([id_review,review_comment,review_rating])
             print(f"Data number {id_review} is saved")
             print([id_review,review_comment,review_rating])
-
-
-"""link_products = "https://www.amazon.com/s?k=sustainability+products&crid=1DOPVMDCV2ZGV&sprefix=sustainability%2Caps%2C256&ref=nb_sb_ss_ts-doa-p_1_14"
-driver.get(link_products)
-
-time.sleep(3)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-# This to scrape all link pages.
-
-racine = soup.findAll("div", class_="sg-col-4-of-2        # print(review)
-        #rate = int(float(i.find("span", class_="a-icon-alt").text.split(' ')[0]))4 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20 gsx-ies-anchor")
-for i in racine:
-    div = i.find("div", class_="a-section a-spacing-base")
-    link_product = div.find('a', href=True)['href']
-    # print(review)
-    # rate = int(float(i.find("span", class_="a-icon-alt").text.split(' ')[0]))
-    print(link_product)"""
-
-""""
-    link_products = "https://www.amazon.com/s?k=sustainability+products&crid=1DOPVMDCV2ZGV&sprefix=sustainability%2Caps%2C256&ref=nb_sb_ss_ts-doa-p_1_14"
-    driver.get(link_products)
-
-    time.sleep(3)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # This to scrape all link pages.
-
-    racine = soup.findAll("div",
-                          class_="sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20 gsx-ies-anchor")
-    for i in racine:
-        div = i.find("div", class_="a-section a-spacing-base")
-        link_product = div.find('a', href=True)['href']
-        # print(review)
-        # rate = int(float(i.find("span", class_="a-icon-alt").text.split(' ')[0]))
-        print(link_product)
-"""
-
-"""link_review = f"https://www.amazon.com/DR-BRIGHT-Eco-Friendly-Biodegradable-Toothbrushes-Compostable/product-reviews/B0CGRJM6LP/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
-
-print(f"link review is {link_review}")
-
-driver.get(link_review)
-response = requests.get(link_review)
-time.sleep(3)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-racine_ = soup.findAll("div", class_="a-section review aok-relative")
-for a in racine_:
-    review_comment = a.find("span", class_="a-size-base review-text review-text-content").text
-    review_rating = a.find("div", class_="a-row").find("span", class_="a-icon-alt").text
-
-    print(f"Data number is saved")
-    print([review_comment, review_rating])
-"""
